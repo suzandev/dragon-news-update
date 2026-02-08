fb0bc5f3c40e6c124a4566661621609548cdb6f9
Suzan Chandra <suzanchandra@Suzans-Mac-mini.local>
2026-02-09 01:12:44 +0600

    add header route with date formate and set theme color and text color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,16 @@ import logo from "../assets/logo.png";
 const Header = () => {
   return (
     <div className="flex justify-center flex-col items-center gap-3 ">
-      <img className="w-87.5" src={logo} alt="Logo" />
-      <p>Journalism Without Fear or Favour</p>
+      <img className="w-96" src={logo} alt="Logo" />
+      <p className="text-accent">Journalism Without Fear or Favour</p>
+      <p className="text-semibold text-accent">
+        {new Date().toLocaleDateString("en-US", {
+          weekday: "long",
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        })}
+      </p>
     </div>
   );
 };
