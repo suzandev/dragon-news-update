e50042c0e877721347319500fab7674c9d8accfa
Suzan Chandra <suzanchandra@Suzans-Mac-mini.local>
2026-02-10 02:25:27 +0600

    CategoryNews data load with loder and set NewsCard. and creating a news card

---
+++ src/components/NewsCard.jsx
@@ -0,0 +1,73 @@
+import { FaEye, FaStar, FaShareAlt, FaRegBookmark } from "react-icons/fa";
+
+const NewsCard = ({ news }) => {
+  const { title, author, thumbnail_url, details, total_view, rating } = news;
+
+  const formattedDate = new Date(author.published_date).toLocaleDateString(
+    "en-US",
+    {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    },
+  );
+  return (
+    <div className="card bg-base-100 shadow-md mb-6">
+      <div className="bg-base-200 flex justify-between items-center p-4">
+        <div className="flex items-center gap-3">
+          <div className="avatar">
+            <div className="w-12 rounded-full">
+              <img src={author.img} alt={author.name} />
+            </div>
+          </div>
+          <div>
+            <h4 className="font-bold text-sm">{author.name}</h4>
+            <p className="text-sm text-gray-500">{formattedDate}</p>
+          </div>
+        </div>
+        <button className="text-gray-500 hover:text-primary flex gap-5">
+          <FaRegBookmark />
+          <FaShareAlt />
+        </button>
+      </div>
+      {/* Title */}
+      <div className="px-4 py-2">
+        <img
+          src={thumbnail_url}
+          alt={title}
+          className="w-full h-48 object-cover rounded-md"
+        />
+      </div>
+
+      {/* details  */}
+      <div className="px-4 text-sm text-gray-700">
+        {details.length > 200 ? (
+          <>
+            {details.slice(0, 200)}...
+            <span className="text-primary font-semibold cursor-pointer hover:underline">
+              Read More
+            </span>
+          </>
+        ) : (
+          details
+        )}
+      </div>
+      {/* Footer  */}
+      <div className="bg-base-200 flex justify-between items-center px-4 py-3 border-t mt-3">
+        {/* Ratting  */}
+        <div className="flex items-center gap-1 text-orange-400">
+          {Array.from({ length: Math.round(rating.number) }).map((_, index) => (
+            <FaStar key={index} />
+          ))}
+          <span className="text-gray-500 text-sm">({rating.number})</span>
+        </div>
+        {/* View  */}
+        <div className="flex items-center gap-2 text-gray-500"></div>
+        <FaEye />
+        <span className="text-sm">{total_view}</span>
+      </div>
+    </div>
+  );
+};
+
+export default NewsCard;
--- src/pages/CategoryNews.jsx
+++ src/pages/CategoryNews.jsx
@@ -1,12 +1,31 @@
-import React from "react";
-import { useParams } from "react-router";
+import { useLoaderData, useParams } from "react-router";
+import NewsCard from "../components/NewsCard";
 
 const CategoryNews = () => {
   const { id } = useParams();
-  console.log("Category ID:", id);
+  const data = useLoaderData();
+
+  let categoryNews = [];
+
+  if (id === "0") {
+    categoryNews = data;
+  } else if (id === "1") {
+    categoryNews = data.filter((news) => news.others.is_today_pick === true);
+  } else {
+    categoryNews = data.filter((news) => news.category_id === Number(id));
+  }
+
   return (
     <div>
-      <h3 className="text-2xl font-bold">Category News Page</h3>
+      <h3 className="font-bold mb-5">
+        Total <span className="text-secondary">{categoryNews.length}</span> News
+        Found
+      </h3>
+      <div className="grid grid-cols-1 gap-5">
+        {categoryNews.map((news) => (
+          <NewsCard key={news.id} news={news}></NewsCard>
+        ))}
+      </div>
     </div>
   );
 };
